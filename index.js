1606ac8e976cf8fca1b737e8746cd2c28d598271
mwacheo <saidmwacheo001@gmail.com>
2024-03-28 00:21:26 +0300

    new Commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Said Omar';
+// const welcomeMessage = currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 4)}!`;
